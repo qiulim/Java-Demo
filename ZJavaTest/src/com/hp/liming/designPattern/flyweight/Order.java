85d184860f4304b98c48aa5f84b2b7d738e49349
qiulim <22792699@qq.com>
2014-11-13 17:40:19 +0800

    享元模式

---
+++ ZJavaTest/src/com/hp/liming/designPattern/flyweight/Order.java
@@ -0,0 +1,16 @@
+package com.hp.liming.designPattern.flyweight;
+
+public abstract class Order {
+
+	/**
+	 * 将咖啡卖给客人
+	 * @param table
+	 */
+	public abstract void serve(Table table);
+	
+	/**
+	 * 返还咖啡名字
+	 * @return
+	 */
+	public abstract String getFlavor();
+}
