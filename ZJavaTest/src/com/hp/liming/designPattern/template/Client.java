66616a4b37681f855bfb7af3b9ab57c02d2a0e9f
qiulim <22792699@qq.com>
2014-11-14 15:46:41 +0800

    模板模式

---
+++ ZJavaTest/src/com/hp/liming/designPattern/template/Client.java
@@ -0,0 +1,14 @@
+package com.hp.liming.designPattern.template;
+
+public class Client {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		ConcreteTemplate ct = new ConcreteTemplate();
+		ct.doAction();
+
+	}
+
+}
---
+++ ZJavaTest/src/com/hp/liming/designPattern/template/ConcreteTemplate.java
@@ -0,0 +1,26 @@
+package com.hp.liming.designPattern.template;
+
+public class ConcreteTemplate extends Template{
+
+	public void doAction() {
+		super.hookMethod();
+	}
+	
+	@Override
+	protected void doAction1() {
+		System.out.println("ConcreteTemplate doAction1()!!!");
+	}
+
+	@Override
+	protected void doAction2() {
+		System.out.println("ConcreteTemplate doAction2()!!!");
+		
+	}
+
+	@Override
+	protected void doAction3() {
+		System.out.println("ConcreteTemplate doAction3()!!!");
+		
+	}
+
+}
