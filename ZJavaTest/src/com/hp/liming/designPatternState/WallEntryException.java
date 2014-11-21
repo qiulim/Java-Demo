f0245f3c7e7a4cf2bbbed20c643a371654f4edfc
qiulim <22792699@qq.com>
2014-11-21 15:34:59 +0800

    状态模式

---
+++ ZJavaTest/src/com/hp/liming/designPatternState/WallEntryException.java
@@ -0,0 +1,15 @@
+package com.hp.liming.designPatternState;
+
+public class WallEntryException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public WallEntryException(String msg) {
+		super(msg);
+		System.out.println(msg);
+	}
+	
+}
